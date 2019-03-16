ab75c18328e332d9d8368d59f68223cf1dc13dcb
Paul Isache <paul-constantin.isache@eon-romania.ro>
2019-03-16 16:26:31 +0200

    Refator modal

--- src/components/SimpleModal.jsx
+++ src/components/SimpleModal.jsx
@@ -3,10 +3,6 @@ import { withStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
 import Button from '@material-ui/core/Button';
 
-function rand() {
-  return Math.round(Math.random() * 20) - 10;
-}
-
 function getModalStyle() {
   const top = 50;
   const left = 50;
