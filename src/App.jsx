00baaae98f07c2befb317f4cbd763bb7e2d7c885
Paul Isache <paul-constantin.isache@eon-romania.ro>
2019-03-16 13:13:48 +0200

    Create login page
    1. Create token context
    2. Use effect and get route query params to get token
    3. Add constats file

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,21 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import Login from './components/Login'
+import Login from './components/Login/'
 
-import logo from './logo.svg';
+import TokenManager from './contexts/token-context'
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/topics">Topics</Link>
-            </li>
-          </ul>
-
-          <hr />
+        <TokenManager>
           <Route exact path="/" component={Login} />
-          <Route path="/about" component={Login} />
+          <Route path="/login" component={Login} />
           <Route path="/topics" component={Login} />
-        </div>
+        </TokenManager>
       </Router>
     );
   }
